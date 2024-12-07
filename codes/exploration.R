b021d03f04cd742c52d2f971775fc5199d113812
Guillo Clément <RK09OA@ad.insee.intra>
2024-12-07 10:57:10 -0400

    changement nom repertoire

--- codes/exploration.R
+++ codes/exploration.R
@@ -117,5 +117,5 @@ map <- leaflet(data_sante_sf) %>%
 # Sauvegarder la carte
 
 saveWidget(map, 
-          file = "output/carte_sante_sxm.html", 
+          file = "docs/carte_sante_sxm.html", 
           selfcontained = TRUE)
